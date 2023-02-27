8273d7d6836037c96a00f9f93526e3a9615c302d
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-02-27 21:08:39 +0100

    Update BadgeGiver.lua

--- BadgeGiver.lua
+++ BadgeGiver.lua
@@ -1,6 +1,6 @@
 script.Parent.Touched:Connect(function(part)
 	if part.Parent:FindFirstChild("Humanoid") then
 		local player = game.Players:GetPlayerFromCharacter(part.Parent)
-		game:GetService("BadgeService"):AwardBadge(player.UserId, 2141603999) -- input badge ID
+		game:GetService("BadgeService"):AwardBadge(player.UserId, "ID") -- input badge ID
 	end
 end)
