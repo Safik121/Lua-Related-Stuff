33eb789889ac9a985e4533d24a040254ec68250f
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-02-27 21:01:42 +0100

    Create RandomSpawn.lua
    Spawns given item at one of designed locations stored in workspace.ItemSpawns, items are taken from ReplicatedStorage.Items folder

---
+++ RandomSpawn.lua
@@ -0,0 +1,23 @@
+local spawns = workspace.ItemSpawns
+local items = game.ReplicatedStorage.Items
+local seconds = 10
+
+local function chooseSpawn()
+    local _spawn = spawns:GetChildren()[math.random(1,#spawns:GetChildren())]
+    return _spawn
+end
+
+local function chooseItem()
+    local item = items:GetChildren()[math.random(1,#items:GetChildren())]
+    return item
+end
+
+while true do
+    wait(seconds)
+    local _spawn = chooseSpawn()
+    local item = chooseItem()
+    local clone = item:Clone()
+
+    clone.Parent = workspace
+    clone.Handle.CFrame = _spawn.CFrame * CFrame.new(0,2.5,0)
+end
