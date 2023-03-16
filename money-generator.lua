651040ea45c75a27f40f0fad8d9e7091068e5ad2
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-03-16 08:32:01 +0100

    Create money-generator.lua
    Add this code to any item and it will generate money as long as owner exists ingame.

---
+++ money-generator.lua
@@ -0,0 +1,33 @@
+local tycoon = script.Parent.Parent.Parent
+local module = require(tycoon.Parent.Parent.Settings)
+local tcnInfo = tycoon:WaitForChild("TycoonInfo")
+local MarketplaceService = game:GetService("MarketplaceService")
+
+-- define how often cash is generated
+local generationInterval = 1
+
+-- function to generate cash
+local function generateCash()
+    local cashValue = 1
+    if tcnInfo.Owner.Value and MarketplaceService:UserOwnsGamePassAsync(tcnInfo.Owner.Value.UserId, module.gamepassId) then
+        cashValue = 2
+    end
+
+    if tcnInfo.Owner.Value == nil then
+        return -- if tycoon belongs to nobody and item somehow exists, don't generate anything
+    end
+    if MarketplaceService:UserOwnsGamePassAsync(tcnInfo.Owner.Value.UserId, module.autoCollect) then
+        local cash = game.ServerStorage:WaitForChild("PlayerCash"):FindFirstChild(tcnInfo.Owner.Value.Name)
+        if cash then
+            cash.Value += cashValue
+        end
+    else
+        tcnInfo.CurrencyToCollect.Value += cashValue
+    end
+end
+
+-- infinite loop until player leaves
+while true do
+    generateCash()
+    wait(generationInterval)
+end
