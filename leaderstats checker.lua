7a35574dcb077be1efed1342f93c5064205ba424
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-03-23 08:52:57 +0100

    Update leaderstats checker.lua

--- leaderstats checker.lua
+++ leaderstats checker.lua
@@ -1,11 +1,10 @@
-local player = game.Player.LocalPlayer
+local player = game:GetService("Players").LocalPlayer
 local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
 local rootpart = character:WaitForChild("RootPart")
 local leaderboard = character:WaitForChild("leaderstats")
 local cash = leaderboard.Cash
-local bool = true
-while bool do
-  print(cash.Value)
-  wait(1)
-end
+
+cash.ValueChanged:Connect(function(newVal)
+    print("Cash:", newVal)
+end)
