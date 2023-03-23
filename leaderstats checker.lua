091d4402e991ccec85bd7c07dca90ec0cc7755a5
Safik121 <105844179+Safik121@users.noreply.github.com>
2023-03-23 08:33:57 +0100

    Update leaderstats checker.lua

--- leaderstats checker.lua
+++ leaderstats checker.lua
@@ -3,8 +3,9 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
 local rootpart = character:WaitForChild("RootPart")
 local leaderboard = character:WaitForChild("leaderstats")
+local cash = leaderboard.Cash
 local bool = true
 while bool do
-  print(leaderboard.Value)
+  print(cash.Value)
   wait(1)
 end
